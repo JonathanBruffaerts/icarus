3803024c56e8cf4724ee4a980938804b20b27e6e
JonathanBruffaerts <114397201+JonathanBruffaerts@users.noreply.github.com>
2026-01-03 05:43:48 +0100

    updated config

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: './',
+  root: '/web2-course-project-front-end-JonathanBruffaerts/',
  
   build: {
     outDir: 'dist',
