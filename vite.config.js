bcac11e26e4f4879a07b4372ac936962302830c7
JonathanBruffaerts <114397201+JonathanBruffaerts@users.noreply.github.com>
2026-08-14 15:03:42 +0200

    update vite.config.js to build multi-page app

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   root: './',  // Repository root
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'admin.html'),
+        studies: resolve(__dirname, 'studies.html')
+      }
+    }
   }
 });
